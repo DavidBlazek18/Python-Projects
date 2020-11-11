5cd897f9f203e70391c0b5ffc1c47389b87a6f6f
david <davidblazek4@gmail.com>
2020-11-11 14:23:46 -0500

    Create python_Assignment_P198,Inheritance.py

---
+++ python_Assignment_P198,Inheritance.py
@@ -0,0 +1,20 @@
+
+class ProTeam:
+        city = "City name: "
+        sport = "Sport name: "
+        color = "Color uniforms: "
+        arena_capacity = int
+
+class Owner(ProTeam):
+        name = " "
+        executive_committee_member = False
+        source_of_wealth = " "               
+
+class Player(ProTeam):
+        name = " "
+        position = " "
+        contract_length = int
+        free_agent = False
+        
+        
+        
